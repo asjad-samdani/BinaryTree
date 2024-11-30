dbbe1f226fada23f8cba75c88fd350a765deab57
Asjad Samdani <samdaniasjad143f@gmail.com>
2024-11-30 17:45:11 +0530

    preorder traversals added

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,38 +1,53 @@
-class BinaryTree {
+public class BinaryTree {
   static class Node {
     int data;
     Node left;
     Node right;
 
-    Node(int data) {
+    public Node(int data) {
       this.data = data;
       this.left = null;
       this.right = null;
     }
-
   }
 
-  class Binary {
-    static int index = -1;
+  static class Binary {
+    static int idx = -1;
 
     public static Node buildTree(int nodes[]) {
-      index++;
-      if (nodes[index] == -1) {
+      idx++;
+
+      if (nodes[idx] == -1) {
+        System.out.println("Returning null for index: " + idx);
+
         return null;
-
       }
-      Node newNode = new Node(nodes[index]);
-      newNode.left = null;
-      newNode.right = null;
-      return newNode;
 
+      Node newNode = new Node(nodes[idx]);
+      System.out.println("Creating node: " + newNode.data);
+
+      newNode.left = buildTree(nodes);
+      newNode.right = buildTree(nodes);
+
+      return newNode;
     }
 
+    public static void preOrder(Node root) {
+      if (root == null) {
+        return;
+      }
+      System.out.print(root.data + " ");
+      preOrder(root.left);
+      preOrder(root.right);
+    }
   }
 
   public static void main(String[] args) {
-    int nodes[] = { 9, 8, -1, 3, 5, -1, 2, -1, -1, 6, 9, 2 };
-    Node BinaryTree = Binary.buildTree(nodes);
-    System.out.println(BinaryTree.data);
+
+    int nodes[] = { 1, 24, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+
+    Node root = Binary.buildTree(nodes);
+
+    Binary.preOrder(root);
   }
 }
